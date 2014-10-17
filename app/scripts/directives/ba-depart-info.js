8135e45bbc51f0c3206bdf31ee6c1275366c345e
gautham stalin <gauthamstalin@gmail.com>
2014-10-17 00:39:30 -0700

    build update

--- app/scripts/directives/ba-depart-info.js
+++ app/scripts/directives/ba-depart-info.js
@@ -3,14 +3,13 @@
 angular.module('nextBartApp')
     .directive('baDepartInfo', function () {
         return {
-            templateUrl: '/views/directives/ba-depart-info.html',
+            templateUrl: 'views/directives/ba-depart-info.html',
             restrict: 'E',
             replace: true,
             scope: {
                 trip: '='
             },
-            link: function postLink(scope, element, attrs) {
-                console.log(scope.trip);
+            link: function postLink() {
             }
         };
     });
