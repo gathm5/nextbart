d6407ec302e03153191cfb9b489c32e6308db47d
Gautham Stalin <Gautham.Stalin@monitise.com>
2014-10-31 16:43:02 -0700

    moment in filter

--- app/scripts/filters/travel-time-filter.js
+++ app/scripts/filters/travel-time-filter.js
@@ -4,17 +4,17 @@ angular.module('nextBartApp')
     .filter('travelTimeFilter', function () {
         return function (trip) {
             if (trip) {
-                var originTime = new Date(trip._origTimeDate + ' ' + trip._origTimeMin);
-                var destTime = new Date(trip._destTimeDate + ' ' + trip._destTimeMin);
-                var diff = destTime - originTime;
-                var diffHrs = Math.round((diff % (86400 * 1000)) / (60 * 60 * 1000)); // hours
+                var originTime = moment(trip._origTimeDate + ' ' + trip._origTimeMin);
+                var destTime = moment(trip._destTimeDate + ' ' + trip._destTimeMin);
+                var diff = destTime.diff(originTime, 'mins');
+                /*var diffHrs = Math.round((diff % (86400 * 1000)) / (60 * 60 * 1000)); // hours
                 var diffMin = Math.round(((diff % (86400 * 1000)) % (60 * 60 * 1000)) / (60 * 1000)); // minutes
 
                 if (diffMin) {
                     return diffMin;
-                }
+                }*/
 
-                return diffHrs * 60;
+                return diff;
             }
         };
     });
