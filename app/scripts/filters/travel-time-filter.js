d6530e7aeb6d81e88af2a72d83d35b1b507cf54a
Gautham Stalin <Gautham.Stalin@monitise.com>
2014-10-31 16:49:43 -0700

    moment mins

--- app/scripts/filters/travel-time-filter.js
+++ app/scripts/filters/travel-time-filter.js
@@ -6,14 +6,7 @@ angular.module('nextBartApp')
             if (trip) {
                 var originTime = moment(trip._origTimeDate + ' ' + trip._origTimeMin);
                 var destTime = moment(trip._destTimeDate + ' ' + trip._destTimeMin);
-                var diff = destTime.diff(originTime, 'mins');
-                /*var diffHrs = Math.round((diff % (86400 * 1000)) / (60 * 60 * 1000)); // hours
-                var diffMin = Math.round(((diff % (86400 * 1000)) % (60 * 60 * 1000)) / (60 * 1000)); // minutes
-
-                if (diffMin) {
-                    return diffMin;
-                }*/
-
+                var diff = destTime.diff(originTime, 'm');
                 return diff;
             }
         };
